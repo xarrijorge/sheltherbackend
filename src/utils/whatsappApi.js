95004af95ec75491fe2cc16b8c7093f6fc218db7
Xarri Jorge <kodinglife@gmail.com>
2024-08-04 02:49:03 +0300

    added whatsapp auth template body

--- src/utils/whatsappApi.js
+++ src/utils/whatsappApi.js
@@ -7,3 +7,38 @@ export const sendEmergencyMessage = async () => {
     const response = await axios.post(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`);
     return response.data;
 }
+
+const authBody = {
+    "messaging_product": "whatsapp",
+    "recipient_type": "individual",
+    "to": "<CUSTOMER_PHONE_NUMBER>",
+    "type": "template",
+    "template": {
+        "name": "<TEMPLATE_NAME>",
+        "language": {
+            "code": "<TEMPLATE_LANGUAGE_CODE>"
+        },
+        "components": [
+            {
+                "type": "body",
+                "parameters": [
+                    {
+                        "type": "text",
+                        "text": "<ONE-TIME PASSWORD>"
+                    }
+                ]
+            },
+            {
+                "type": "button",
+                "sub_type": "url",
+                "index": "0",
+                "parameters": [
+                    {
+                        "type": "text",
+                        "text": "<ONE-TIME PASSWORD>"
+                    }
+                ]
+            }
+        ]
+    }
+}
